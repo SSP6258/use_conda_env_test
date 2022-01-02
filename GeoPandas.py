9258fb67c96bb6d462ad6dc5fe9509ec64591d50
SSP6258 <ssp6258@yahoo.com.tw>
2022-01-02 18:41:46 +0800

    App update

--- GeoPandas.py
+++ GeoPandas.py
@@ -1,6 +1,7 @@
 import os.path
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import pandas as pd
 from shapely.geometry import shape, Point
 import streamlit as st
 
@@ -37,32 +38,33 @@ def fn_geo_test():
     return shapes, properties
 
 
-def fn_search(x, y, shapes, properties):
+def fn_search(lon, lat, shapes, properties):
     coor_2_vill = 'Uknown1'
     vill = 'Unknown'
 
     for k in shapes.keys():
         # print(k, properties[k])
-        if shapes[k].contains(Point(x, y)):
+        if shapes[k].contains(Point(lon, lat)):
             vill = properties[k]
-            coor_2_vill = f'{x}, {y} is in {vill}'
+            coor_2_vill = f'{lon}, {lat} is in {vill}'
 
             x, y = shapes[k].exterior.xy
-            plt.plot(x, y, c="red")
-            plt.show()
+            fig = plt.figure()
+            plt.plot(x, y, c="green")
+            plt.plot(lon, lat, c="red", marker='X')
             break
 
     print(coor_2_vill)
 
-    return vill
+    return vill, fig
 
 
 def fn_coor_2_vill(lon, lat):
     shapes, properties = fn_geo_test()
 
-    vill = fn_search(lon, lat, shapes, properties)
+    vill, fig = fn_search(lon, lat, shapes, properties)
 
-    return vill
+    return vill, fig
 
 
 def fn_st_test():
@@ -70,11 +72,23 @@ def fn_st_test():
     lon = st.text_input('經度:', value="121.531014")
     lat = st.text_input('緯度:', value="25.102158")
     is_click = st.button('按我查詢')
+    path = '.\\database'
+    output = 'coor_2_vill.csv'
+    file = os.path.join(path, output)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     if is_click:
-        vill = fn_coor_2_vill(float(lon), float(lat))
-        st.subheader('此座標的行政區是:')
-        st.write(vill)
+        vill, fig = fn_coor_2_vill(float(lon), float(lat))
+        st.write(' ')
+        st.subheader(f'此座標的行政區是: {vill}')
+        st.write(' ')
+        st.pyplot(fig)
+
+        check = f'{lon}_{lat}_{vill}'
+        dic_coor = dict(lon=[lon], lat=[lat], vill=[vill], check=[check])
+        df = pd.DataFrame(dic_coor, index=None)
+        df.to_csv(file)
 
 
 def fn_main():
