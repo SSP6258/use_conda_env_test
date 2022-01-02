0bb299b2b049b896caf45c64949dcb204088e90e
SSP6258 <ssp6258@yahoo.com.tw>
2022-01-02 10:33:45 +0800

    App update

--- GeoPandas.py
+++ GeoPandas.py
@@ -6,12 +6,12 @@ import streamlit as st
 
 
 def fn_geo_test():
-    path = r'D:\02_Project\proj_python\use_conda_env\mapdata202104280245'
+    path = r'.\mapdata202104280245'
     file = r'TOWN_MOI_1100415.shp'
     g = os.path.join(path, file)
     gis_d = gpd.read_file(g, encoding='utf-8')
 
-    path = r'D:\02_Project\proj_python\use_conda_env\mapdata202112240331'
+    path = r'.\mapdata202112240331'
     file = r'VILLAGE_MOI_1101214.shp'
     g = os.path.join(path, file)
     gis_v = gpd.read_file(g, encoding='utf-8')
